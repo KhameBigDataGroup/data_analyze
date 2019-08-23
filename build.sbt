b97ca8c2af1ab31cc38c9f782d17d0895ad395c2
mjshariati98 <mjshariati98@gmail.com>
2019-08-23 16:13:50 +0430

    [ADD] Test writing to HDFS

---
+++ build.sbt
@@ -0,0 +1,33 @@
+
+name := "khame_data_analyze"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.2"
+
+// https://mvnrepository.com/artifact/org.apache.kafka/kafka
+//libraryDependencies += "org.apache.kafka" %% "kafka" % "2.3.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-8
+//libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-8" % "2.4.3"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
+//libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.4.3"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
+libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.3"
+
+
+//libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.3.0"
+
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/App.scala
@@ -0,0 +1,67 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object App {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .set("spark.driver.allowMultipleContexts", "true")
+      .set("spark.sql.streaming.checkpointLocation","./checkpoints")
+    //      .set("spark.driver.memory","500m")
+    //      .set("spark.executor.memory","500m")
+    //      .set("spark.executor.memoryOverhead","100m")
+    //      .set("spark.executor.cores","1")
+    //      .set("spark.cores.max","1")
+    //      .set("spark.executor.instances","1")
+
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .config(sparkConf).appName("khame")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+
+//    val df = spark
+//      .readStream
+//      .format("kafka")
+//      .option("kafka.bootstrap.servers", "127.0.0.1:9092")
+//      .option("subscribe", "first_topic")
+//      .option("startingOffsets", "earliest")
+//      .load()
+//
+//    import spark.implicits._
+//
+//    val cast = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+//      .as[(String, String)]
+
+
+//    val query1 = df.collect.foreach(println)
+//      .writeStream
+//      .format("kafka")
+//      .start()
+//    val query2 = df.writeStream
+//      .format("kafka")
+//      .start()
+//
+//    df.printSchema()
+////    query1.awaitTermination()
+//    query2.awaitTermination()
+
+//    val query = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic")
+//      .as[(String, String, String)]
+//      .writeStream
+//      .outputMode("append")
+//      .format("console")
+//      .start()
+//    query.awaitTermination()
+//
+//    df.write.parquet("hdfs://localhost:50070/test/hello.txt")
+//    df.write.save("hdfs://localhost:50070/test")
+
+    val numbersRdd = sc.parallelize((1 to 10000).toList)
+    numbersRdd.saveAsTextFile("hdfs://localhost:8020/test/numbers-as-text-2")
+
+
+    }
+}
