d8955262872cdfe6c8afb5c8c559c10f60ef60ec
mjshariati98 <mjshariati98@gmail.com>
2019-08-24 15:32:06 +0430

    test_read_kafka

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,42 +1,80 @@
-import java.net.URI
-
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object App extends App {
   val sparkConf = new SparkConf()
-      .setAppName("App")
-      //    .setMaster("localhost:7077")
-      .setMaster("local[*]")
-      .set("spark.driver.allowMultipleContexts", "true")
-      .set("spark.sql.streaming.checkpointLocation", "./checkpoints")
+    .setAppName("App")
+    //    .setMaster("localhost:7077")
+    .setMaster("local[*]")
+    .set("spark.driver.allowMultipleContexts", "true")
+    .set("spark.sql.streaming.checkpointLocation", "./checkpoints")
 
-//      .set("spark.driver.memory", "500m")
-//      .set("spark.executor.memory", "500m")
-//      .set("spark.executor.memoryOverhead", "100m")
-//      .set("spark.executor.cores", "4")
-//      .set("spark.cores.max", "4")
-//      .set("spark.executor.instances", "1")
+    .set("spark.driver.memory", "500m")
+    .set("spark.executor.memory", "500m")
+    .set("spark.executor.memoryOverhead", "100m")
+    .set("spark.executor.cores", "4")
+    .set("spark.cores.max", "4")
+    .set("spark.executor.instances", "1")
 
-    val spark = SparkSession
-      .builder()
-      .master("local")
-      .config(sparkConf)
-      .appName("App")
-      .getOrCreate()
+  val spark = SparkSession
+    .builder()
+    .master("local")
+    .config(sparkConf)
+    .appName("App")
+    .getOrCreate()
 
-    val sc = spark.sparkContext
+  val sc = spark.sparkContext
+
+  val df = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "172.17.0.1:9092")
+    .option("subscribe", "bitcoin")
+    .option("startingOffsets", "earliest")
+    .load()
+  //
+  import spark.implicits._
+  //
+  //    val cast = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+  //      .as[(String, String)]
+
+
+  //    val query1 = df.collect.foreach(println)
+  //      .writeStream
+  //      .format("kafka")
+  //      .start()
+  //    val query2 = df.writeStream
+  //      .format("kafka")
+  //      .start()
+  //
+  //    df.printSchema()
+  ////    query1.awaitTermination()
+  //    query2.awaitTermination()
+
+  val query = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic")
+    .as[(String, String, String)]
+    .writeStream
+    .outputMode("append")
+    .format("console")
+    .start()
+  query.awaitTermination()
+  //
+
+
+//  df.rdd.map(_.toSeq.map(_.toString).mkString(",")).saveAsTextFile("hdfs://localhost:8020/test/kafka")
 
 
 
-  val hdfs = FileSystem.get(new URI("hdfs://localhost:8020/"), new Configuration())
-  val path = new Path("/tmp/numbers-as-text")
-  val stream = hdfs.open(path)
-  def readLines = Stream.cons(stream.readLine, Stream.continually( stream.readLine))
+//  df.write.parquet("hdfs://localhost:8020/test/kafka")
+  //    df.write.save("hdfs://localhost:50070/test")
 
-  //This example checks line for null and prints every existing line consequentally
-  readLines.takeWhile(_ != null).foreach(line => println(line))
+  //  val numbersRdd = sc.parallelize((1 to 10000).toList)
+  //  numbersRdd.saveAsTextFile("hdfs://localhost:8020/test/numbers-as-text-2")
 
+  //  sc.stop()
+
+
+//  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "topic")
+//    .write
+//    .format("hdfs")
 }
